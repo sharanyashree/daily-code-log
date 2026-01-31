3d97aba74ce6f271214d0a9c437fbcaab7d19001
Sharanya Shree <sharanyasadananda2005@gmail.com>
2026-01-31 21:12:29 +0530

    Create reversing_numbers.java

---
+++ DSA/reversing_numbers.java
@@ -0,0 +1,14 @@
+class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 1234;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Reversed number = " + reverse);
+    }
+}
